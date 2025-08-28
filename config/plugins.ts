b41a9e3833af7b383bee457ec6eb65c3664d4a75
neeraj sharma <neeraj@vadavision.ca>
2025-08-28 17:24:47 +0530

    feat: Configure security middleware for S3 image previews
    - Update Content Security Policy to allow S3 bucket access
    - Add img-src and media-src directives for well-md-storage.s3.us-east-1.amazonaws.com
    - Enable image thumbnails in Strapi Media Library
    - Add ACL and signedUrlExpires parameters to S3 configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,12 +3,12 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        s3Options: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-          region: env('AWS_REGION'),
-        },
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+        region: env('AWS_REGION'),
         params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
           Bucket: env('AWS_S3_BUCKET'),
         },
       },
